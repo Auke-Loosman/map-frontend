e77ea3d8d121b1b888b03b5b4abbdfd28a31cf5f
Auke <auke@skrepr.com>
2026-03-09 13:21:00 +0100

    Fix router configuration and register Vuetify components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Map Item Manager</h1>
+  <v-container>
+    <v-card>
+      <v-card-title>Map Item Manager</v-card-title>
+
+      <v-card-text> Frontend connected and ready. </v-card-text>
+    </v-card>
+  </v-container>
 </template>

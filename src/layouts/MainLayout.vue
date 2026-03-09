22580e5ceafb5b2ba2d67a3a8bfac27e57190a32
Auke <auke@skrepr.com>
2026-03-09 14:07:55 +0100

    feat(ui): add authentication navigation to app bar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
 import { useAuth } from '../composables/useAuth'
+import { useRouter } from 'vue-router'
 
 const { user, logout } = useAuth()
+const router = useRouter()
+
+function handleLogout() {
+  logout()
+  router.push('/login')
+}
 </script>
 
 <template>
   <v-app>
-    <v-app-bar color="primary" dark>
+    <v-app-bar color="primary">
       <v-app-bar-title> Map Item Manager </v-app-bar-title>
 
       <v-spacer />
 
-      <div v-if="user" class="mr-4">
-        {{ user.email }}
-      </div>
+      <template v-if="!user">
+        <v-btn variant="text" to="/login"> Login </v-btn>
+        <v-btn variant="text" to="/register"> Register </v-btn>
+      </template>
 
-      <v-btn v-if="user" variant="text" @click="logout"> Logout </v-btn>
+      <template v-else>
+        <div class="mr-4">
+          {{ user.email }}
+        </div>
+
+        <v-btn variant="text" @click="handleLogout"> Logout </v-btn>
+      </template>
     </v-app-bar>
 
     <v-main>
